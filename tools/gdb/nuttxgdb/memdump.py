d29d5a6499810cef3e92e56876af95abfd7e7a88
xuxingliang <xuxingliang@xiaomi.com>
2024-11-21 20:32:30 +0800

    gdb/memdump: reorganize the memdump parameters
    Make the dump_nodes etc more common to use.
    
    Signed-off-by: xuxingliang <xuxingliang@xiaomi.com>

--- tools/gdb/nuttxgdb/memdump.py
+++ tools/gdb/nuttxgdb/memdump.py
@@ -149,6 +149,320 @@ def mm_dumpnode(node, count, align, simple, detail, alive):
                 align,
                 charnode + mm_allocnode_type.sizeof,
             )
+
+def filter_node(
+    pid=None,
+    nodesize=None,
+    used=None,
+    free=None,
+    seqmin=None,
+    seqmax=None,
+    orphan=None,
+    no_pid=None,
+    no_heap=None,
+    no_pool=None,
+) -> bool:
+    return lambda node: (
+        (pid is None or node.pid == pid)
+        and (no_pid is None or node.pid != no_pid)
+        and (nodesize is None or node.nodesize == nodesize)
+        and (not used or not node.is_free)
+        and (not free or node.is_free)
+        and (seqmin is None or node.seqno >= seqmin)
+        and (seqmax is None or node.seqno <= seqmax)
+        and (not orphan or node.is_orphan)
+    )
+
+
+def dump_nodes(
+    heaps: List[mm.MMHeap],
+    filters=None,
+) -> Generator[MMNodeDump, None, None]:
+    no_heap = filters and filters.get("no_heap")
+    no_pool = filters and filters.get("no_heap")
+
+    if not no_heap:
+        yield from (
+            node
+            for heap in heaps
+            for node in filter(filter_node(**filters), heap.nodes)
+        )
+
+    if not no_pool:
+        yield from (
+            blk
+            for pool in mm.get_pools(heaps)
+            for blk in filter(filter_node(**filters), pool.blks)
+        )
+
+
+def group_nodes(
+    nodes: List[MMNodeDump], grouped: Dict[MMNodeDump, List[MMNodeDump]] = None
+) -> Dict[MMNodeDump, List[MMNodeDump]]:
+    grouped = grouped or defaultdict(list)
+    for node in nodes:
+        grouped[node].append(node)
+    return grouped
+
+
+def print_node(node: MMNodeDump, alive, count=1, formatter=None, no_backtrace=False):
+    formatter = (
+        formatter or "{:>1} {:>4} {:>12} {:>12} {:>12} {:>9} {:>14} {:>18} {:}\n"
+    )
+    gdb.write(
+        formatter.format(
+            "\x1b[33;1m*\x1b[m" if not alive else "",
+            "P" if node.from_pool else "H",
+            count,
+            node.pid,
+            node.nodesize,
+            node.overhead,
+            node.seqno,
+            hex(node.address),
+            "",
+        )
+    )
+
+    if mm.CONFIG_MM_BACKTRACE and not no_backtrace:
+        leading = formatter.format("", "", "", "", "", "", "", "", "")[:-1]
+        btformat = leading + "{1:<48}{2}\n"
+        if node.backtrace and node.backtrace[0]:
+            gdb.write(f"{utils.Backtrace(node.backtrace, formatter=btformat)}\n")
+
+
+def print_header(formatter=None):
+    formatter = (
+        formatter or "{:>1} {:>4} {:>12} {:>12} {:>12} {:>9} {:>14} {:>18} {:}\n"
+    )
+    head = (
+        "",
+        "Pool",
+        "CNT",
+        "PID",
+        "Size",
+        "Overhead",
+        "Seqno",
+        "Address",
+        "Backtrace",
+    )
+    gdb.write(formatter.format(*head))
+
+
+def get_heaps(args_heap=None) -> List[mm.MMHeap]:
+    if args_heap:
+        return [mm.MMHeap(gdb.parse_and_eval(args_heap))]
+    return mm.get_heaps()
+
+
+class MMDump(gdb.Command):
+    """Dump memory manager heap"""
+
+    def __init__(self):
+        super().__init__("mm dump", gdb.COMMAND_USER)
+        # define memdump as mm dump
+        utils.alias("memdump", "mm dump")
+
+    def parse_args(self, arg):
+        parser = argparse.ArgumentParser(description=self.__doc__)
+        parser.add_argument(
+            "-a",
+            "--address",
+            type=str,
+            default=None,
+            help="Find the node that contains the address and exit",
+        )
+
+        parser.add_argument(
+            "--heap",
+            type=str,
+            default=None,
+            help="Which heap to inspect",
+        )
+
+        parser.add_argument(
+            "-p", "--pid", type=int, default=None, help="Thread PID, -1 for mempool"
+        )
+        parser.add_argument(
+            "-i", "--min", type=int, default=None, help="Minimum sequence number"
+        )
+        parser.add_argument(
+            "-x", "--max", type=int, default=None, help="Maximum sequence number"
+        )
+        parser.add_argument("--free", action="store_true", help="Free flag")
+        parser.add_argument("--biggest", action="store_true", help="biggest allocated")
+        parser.add_argument(
+            "--orphan", action="store_true", help="Filter nodes that are orphan"
+        )
+        parser.add_argument(
+            "--top", type=int, default=None, help="biggest top n, default to all"
+        )
+        parser.add_argument(
+            "--size", type=int, default=None, help="Node block size filter."
+        )
+        parser.add_argument(
+            "--no-pool",
+            "--nop",
+            action="store_true",
+            help="Exclude dump from memory pool",
+        )
+        parser.add_argument(
+            "--no-heap", "--noh", action="store_true", help="Exclude heap dump"
+        )
+        parser.add_argument(
+            "--no-group", "--nog", action="store_true", help="Do not group the nodes"
+        )
+        parser.add_argument(
+            "--no-backtrace",
+            "--nob",
+            action="store_true",
+            help="Do not print backtrace",
+        )
+        parser.add_argument(
+            "--no-reverse",
+            "--nor",
+            action="store_true",
+            help="Do not reverse the sort result",
+        )
+
+        # add option to sort the node by size or count
+        parser.add_argument(
+            "--sort",
+            type=str,
+            choices=["size", "nodesize", "count", "seq", "address"],
+            default="count",
+            help="sort the node by size(nodesize * count), nodesize,  count or sequence number",
+        )
+
+        try:
+            return parser.parse_args(gdb.string_to_argv(arg))
+        except SystemExit:
+            return
+
+    def find_address(self, addr, heap=None, log=None):
+        """Find the node that contains the address from memdump log or live dump."""
+        addr = int(gdb.parse_and_eval(addr))
+        heaps = [mm.MMHeap(gdb.parse_and_eval(heap))] if heap else mm.get_heaps()
+
+        # Find pool firstly
+        node = next(
+            (blk for pool in mm.get_pools(heaps) if (blk := pool.find(addr))), None
+        )
+
+        # Try heap if not found in pool
+        node = node or next((node for heap in heaps if (node := heap.find(addr))), None)
+
+        return addr, node
+
+    def collect_nodes(self, heap, log=None, filters=None):
+        heaps = [mm.MMHeap(gdb.parse_and_eval(heap))] if heap else mm.get_heaps()
+        nodes = dump_nodes(heaps, filters)
+
+        return nodes
+
+    def invoke(self, arg: str, from_tty: bool) -> None:
+        if not (args := self.parse_args(arg)):
+            return
+
+        print_header()
+
+        pids = [int(tcb["pid"]) for tcb in utils.get_tcbs()]
+
+        def printnode(node, count):
+            print_node(node, node.pid in pids, count, no_backtrace=args.no_backtrace)
+
+        # Find the node by address, find directly and then quit
+        if args.address:
+            addr, node = self.find_address(args.address, args.heap, args.log)
+            if not node:
+                print(f"Address {addr:#x} not found in any heap")
+            else:
+                source = "Pool" if node.from_pool else "Heap"
+                printnode(node, 1)
+                print(f"{addr: #x} found belongs to {source} - {node}")
+
+            if node.prevnode:
+                print(f"prevnode: {node.prevnode}")
+            if node.nextnode:
+                print(f"nextnode: {node.nextnode}")
+            return
+
+        filters = {
+            "pid": args.pid,
+            "nodesize": args.size,
+            "used": not args.free,
+            "free": args.free,
+            "seqmin": args.min,
+            "seqmax": args.max,
+            "orphan": args.orphan,
+            "no_heap": args.no_heap,
+            "no_pool": args.no_pool,
+        }
+
+        nodes = self.collect_nodes(args.heap, log=args.log, filters=filters)
+
+        sort_method = {
+            "count": lambda node: 1,
+            "size": lambda node: node.nodesize,
+            "nodesize": lambda node: node.nodesize,
+            "seq": lambda node: node.seqno,
+            "address": lambda node: node.address,
+        }
+
+        def sort_nodes(nodes, sort=None):
+            sort = sort or args.sort
+            nodes = sorted(nodes, key=sort_method[sort], reverse=not args.no_reverse)
+            if args.top is not None:
+                nodes = nodes[: args.top] if args.top > 0 else nodes[args.top :]
+            return nodes
+
+        if args.biggest:
+            # Dump the biggest node is same as sort by nodesize and do not group them
+            args.sort = "nodesize"
+            args.no_group = True
+
+        if args.no_group:
+            # Print nodes without grouping
+            nodes = list(nodes)
+
+            for node in sort_nodes(nodes):
+                printnode(node, 1)
+
+            gdb.write(f"Total blks: {len(nodes)}\n")
+        else:
+            # Group the nodes and then print
+
+            grouped: Dict[MMNodeDump, MMNodeDump] = defaultdict(list)
+            grouped = group_nodes(nodes)
+
+            # Replace the count and size to count grouped nodes
+            sort_method["count"] = lambda node: len(grouped[node])
+            sort_method["size"] = lambda node: node.nodesize * len(grouped[node])
+            total_blk = total_size = 0
+            for node in sort_nodes(grouped.keys()):
+                count = len(grouped[node])
+                total_blk += count
+                if node.pid != mm.PID_MM_MEMPOOL:
+                    total_size += count * node.nodesize
+                printnode(node, count)
+
+            print(f"Total {total_blk} blks, {total_size} bytes")
+
+
+class MMfrag(gdb.Command):
+    """Show memory fragmentation rate"""
+
+    def __init__(self):
+        super().__init__("mm frag", gdb.COMMAND_USER)
+        utils.alias("memfrag", "mm frag")
+
+    def invoke(self, args, from_tty):
+        parser = argparse.ArgumentParser(description=self.__doc__)
+        parser.add_argument(
+            "--heap",
+            type=str,
+            default=None,
+            help="Which heap to inspect",
+>>>>>>> 5615c252f0 (gdb/memdump: reorganize the memdump parameters)
         )
 
     gdb.write("\n")
