1151e4a26b2bcc87424ae72ea9b78c0aa187abbf
Bowen Wang <wangbowen6@xiaomi.com>
2025-01-09 00:39:38 +0800

    nxgdb/rpmsg.py: support dump the rpmsg virtio transport
    VELAPLATFO-51875
    
    Now we can dump the rpmsg virtio transport virtqueue.
    If only need dump the transport, use:
    (gdb) rpmsgdump -t
    Rpmsg Device: rpmsg:0x184dbeb8 rdev:0x184dbf34
    Rpmsg Virtio Trasport: rvdev:0x184dbf34 h2r_buf_size:512 r2h_buf_size:512
    Rpmsg Virtqueues:
    vq           name     size  free  queued  desc_head_idx  available_idx  avail.idx  avail.flags  used_cons_idx  used.idx  used.flags
    --           ----     ----  ----  ------  -------------  -------------  ---------  -----------  -------------  --------  ----------
    0x18598da0   tx_vq    8     1     0       7              0              2233       0            2226           2233      0
    0x18598e30   rx_vq    8     0     0       32768          0              1704       0            1696           1696      0
    
    Change-Id: If291ec7511ce4fdae2f940e365fba587a35b859b
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- tools/pynuttx/nxgdb/rpmsg.py
+++ tools/pynuttx/nxgdb/rpmsg.py
@@ -20,6 +20,8 @@
 #
 ############################################################################
 
+import argparse
+
 import gdb
 
 from . import utils
@@ -39,13 +41,88 @@ class RPMsgDump(gdb.Command):
         "ns_bound_cb",
         "ns_unbind_cb",
     ]
+    VQ_HEADER = [
+        "vq",
+        "name",
+        "size",
+        "free",
+        "queued",
+        "desc_head_idx",
+        "available_idx",
+        "avail.idx",
+        "avail.flags",
+        "used_cons_idx",
+        "used.idx",
+        "used.flags",
+    ]
 
     CALLBACK_FORAMTTER = "{:<20} {:<40} {:<40}"
     ENDPOINT_FORMATTER = "{:<20} {:<20} {:<12} {:<12} {:<40} {:<40} {:<40}"
+    VQ_FORMATTER = (
+        "{:<12} {:<8} {:<5} {:<5} {:<7} {:<14} {:<14} {:<10} {:<12} {:<14} {:<9} {:<11}"
+    )
 
     def __init__(self):
         if utils.get_symbol_value("CONFIG_RPMSG"):
             super(RPMsgDump, self).__init__("rpmsgdump", gdb.COMMAND_USER)
+
+    def parse_args(self, arg):
+        parser = argparse.ArgumentParser(description=self.__doc__)
+        parser.add_argument(
+            "-t",
+            "--transport-only",
+            action="store_true",
+            help="Only dump the rpmsg transport layer",
+        )
+
+        try:
+            return parser.parse_args(gdb.string_to_argv(arg))
+        except SystemExit:
+            return
+
+    def dump_virtqueue(self, vq):
+        vr = vq["vq_ring"]
+        gdb.write(
+            self.VQ_FORMATTER.format(
+                f"{vq}",
+                f"{vq['vq_name'].string()}",
+                f"{vq['vq_nentries']}",
+                f"{vq['vq_free_cnt']}",
+                f"{vq['vq_queued_cnt']}",
+                f"{vq['vq_desc_head_idx']}",
+                f"{vq['vq_available_idx']}",
+                f"{vr['avail']['idx']}",
+                f"{vr['avail']['flags']}",
+                f"{vq['vq_used_cons_idx']}",
+                f"{vr['used']['idx']}",
+                f"{vr['used']['flags']}",
+            )
+            + "\n"
+        )
+
+    def dump_rpmsg_virtio(self, rdev):
+        real_addr = hex(
+            gdb.lookup_symbol("rpmsg_virtio_get_tx_payload_buffer")[0].value().address
+        )
+        ops_addr = hex(int(utils.Value(rdev["ops"]["get_tx_payload_buffer"])) & ~1)
+        if real_addr != ops_addr:
+            return
+
+        rvdev = rdev.cast(utils.lookup_type("struct rpmsg_virtio_device").pointer())
+
+        gdb.write(
+            f"Rpmsg Virtio Trasport: rvdev:{rvdev} h2r_buf_size:{rvdev['config']['h2r_buf_size']}"
+            f"r2h_buf_size:{rvdev['config']['r2h_buf_size']}\n"
+        )
+        for vbuff in NxList(rvdev["reclaimer"], "struct vbuff_reclaimer_t", "node"):
+            gdb.write(f"rvdev reclaimer vbuff:{vbuff} idx:{vbuff['idx']}")
+
+        gdb.write("Rpmsg Virtqueues:\n")
+        self.print_headers(self.VQ_HEADER, self.VQ_FORMATTER)
+        self.dump_virtqueue(rvdev["svq"])
+        self.dump_virtqueue(rvdev["rvq"])
+
+        gdb.write("\n")
 
     def print_headers(self, headers, formatter):
         gdb.write(formatter.format(*headers) + "\n")
@@ -79,13 +156,11 @@ class RPMsgDump(gdb.Command):
         )
 
     def dump_rdev(self, rdev):
-        gdb.write(f"device:{rdev}\n")
-
         self.dump_rdev_bitmap(rdev)
         self.dump_rdev_epts(rdev["endpoints"])
 
     def dump_rpmsg_cb(self):
-        gdb.write("g_rpmsg_cb:\n")
+        gdb.write("Rpmsg Callback:\n")
         self.print_headers(self.CALLBACK_HEADER, self.CALLBACK_FORAMTTER)
 
         output = []
@@ -97,11 +172,17 @@ class RPMsgDump(gdb.Command):
             )
         gdb.write("\n".join(output) + "\n")
 
-    def dump_rpmsg(self):
-        gdb.write("g_rpmsg:\n")
+    def dump_rpmsg(self, transport):
         for rpmsg in NxList(gdb.parse_and_eval("g_rpmsg"), "struct rpmsg_s", "node"):
-            self.dump_rdev(rpmsg["rdev"])
+            gdb.write(f"Rpmsg Device: rpmsg:{rpmsg} rdev:{rpmsg['rdev']}\n")
+            if not transport:
+                self.dump_rdev(rpmsg["rdev"])
+            self.dump_rpmsg_virtio(rpmsg["rdev"])
 
     def invoke(self, args, from_tty):
-        self.dump_rpmsg_cb()
-        self.dump_rpmsg()
+        if not (args := self.parse_args(args)):
+            return
+
+        if not args.transport:
+            self.dump_rpmsg_cb()
+        self.dump_rpmsg(args.transport)
