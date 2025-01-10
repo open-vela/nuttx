59bb9b7555ab89579b71349d75109a6ec9383636
xuxingliang <xuxingliang@xiaomi.com>
2025-01-10 12:59:16 +0800

    nxgdb/tests: fix thread related tests
    VELAPLATFO-43422
    
    We should use info nxthreads, and nxthread correpondingly if thread
    awareness support is not sure.
    
    In this way, when the new target like `target stub` support thread awareness,
    we can use info threads. Hard wire info thread to info nxthread makes it impossible.
    
    Change-Id: I22b2ffcad68a707f18cad7f5cb29791485a3ffe4
    Signed-off-by: xuxingliang <xuxingliang@xiaomi.com>

--- tools/pynuttx/tests/test_runtime_thread.py
+++ tools/pynuttx/tests/test_runtime_thread.py
@@ -33,36 +33,36 @@ class TestThread(unittest.TestCase):
         pass
 
     def test_info_threads(self):
-        gdb.execute("info threads")
+        gdb.execute("info nxthreads")
 
     def test_thread(self):
-        gdb.execute("thread")
+        gdb.execute("nxthread")
 
     def test_thread_bad_args(self):
-        out = gdb.execute("thread apply", to_string=True)
+        out = gdb.execute("nxthread apply", to_string=True)
         self.assertEqual(out, "Please specify a thread ID list\n")
 
-        out = gdb.execute("thread apply bad", to_string=True)
+        out = gdb.execute("nxthread apply bad", to_string=True)
         self.assertEqual(out, "Please specify a command following the thread ID list\n")
 
-        out = gdb.execute("thread apply badid0 cmd", to_string=True)
+        out = gdb.execute("nxthread apply badid0 cmd", to_string=True)
         self.assertEqual(out, "Please specify a thread ID list and command\n")
 
-        out = gdb.execute("thread badid", to_string=True)
+        out = gdb.execute("nxthread badid", to_string=True)
         self.assertEqual(out, "Invalid thread id badid\n")
 
-        out = gdb.execute("thread 256", to_string=True)
+        out = gdb.execute("nxthread 256", to_string=True)
         self.assertEqual(out, "Invalid thread id 256\n")
 
     def test_thread_apply_all(self):
-        out = gdb.execute("thread apply all bt", to_string=True)
+        out = gdb.execute("nxthread apply all bt", to_string=True)
         self.assertTrue("#0" in out and "Thread" in out, msg=f"Got: {out}")
 
     def test_thread_apply_with_ids(self):
-        out = gdb.execute("thread apply 0 bt", to_string=True)
+        out = gdb.execute("nxthread apply 0 bt", to_string=True)
         self.assertTrue("#0" in out and "Thread 0" in out, msg=f"Got: {out}")
 
-        out = gdb.execute("thread apply 0 1 info reg", to_string=True)
+        out = gdb.execute("nxthread apply 0 1 info reg", to_string=True)
         # info reg has format like
         # reg      xxx      xxx
         self.assertGreaterEqual(len(out.split("\n")[0].split()), 2, msg=f"Got: {out}")
@@ -71,7 +71,7 @@ class TestThread(unittest.TestCase):
         # This command suppose to switch the stack frames
         # make sure we have at least two threads
         if gdb.parse_and_eval("g_npidhash") < 2:
-            out = gdb.execute("thread 0", to_string=True)
+            out = gdb.execute("nxthread 0", to_string=True)
             self.assertEqual(out, "")
             return
 
@@ -82,13 +82,13 @@ class TestThread(unittest.TestCase):
 
         cur_thread_id = gdb.parse_and_eval("g_running_tasks")["pid"]
 
-        gdb.execute("thread 0")
+        gdb.execute("nxthread 0")
         gdb.execute("frame 0")
         cur_sp = utils.get_sp()
 
         sps = []
         for i in range(gdb.parse_and_eval("g_npidhash")):
-            gdb.execute(f"thread {i}")
+            gdb.execute(f"nxthread {i}")
             # switch frame here
             gdb.execute("frame 0")
             new_sp = utils.get_sp()
@@ -97,4 +97,4 @@ class TestThread(unittest.TestCase):
         # We should have some different stack pointers
         self.assertFalse(all([int(sp) == int(cur_sp) for sp in sps]))
 
-        gdb.execute(f"thread {cur_thread_id}")
+        gdb.execute(f"nxthread {cur_thread_id}")
