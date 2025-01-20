66981a8a8494722f75328cdb3f5de0db6d91182f
xuxingliang <xuxingliang@xiaomi.com>
2025-01-20 22:16:12 +0800

    nxgdb/libuv: fix path in comment
    VELAPLATFO-45245
    
    Change-Id: I1e2c7269b8bab7b7c866fa6f19c3cf9ddcd07493
    Signed-off-by: xuxingliang <xuxingliang@xiaomi.com>

--- tools/pynuttx/nxgdb/libuv.py
+++ tools/pynuttx/nxgdb/libuv.py
@@ -1,5 +1,5 @@
 ############################################################################
-# apps/system/libuv/gdb/libuv.py
+# tools/pynuttx/nxgdb/libuv.py
 #
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
