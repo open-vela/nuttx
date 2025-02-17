888357935f669320c754130e0f4b1af55de51760
yangao1 <yangao1@xiaomi.com>
2025-02-17 21:22:43 +0800

    pynuttx/elf.py:add ELFParser class from nxstub/utils.py
    VELAPLATFO-53625
    
    Change-Id: Ice64b66a8c80851b4db022a52096dd38d5ab353f
    Signed-off-by: yangao1 <yangao1@xiaomi.com>

--- tools/pynuttx/nxelf/elf.py
+++ tools/pynuttx/nxelf/elf.py
@@ -22,18 +22,38 @@
 import logging
 import time
 from enum import IntEnum
+from typing import Tuple
 
 from .macros import Macro
 
 try:
-    import construct
     import cxxfilt
+    import lief
+    from construct import (
+        Array,
+        Construct,
+        Float16l,
+        Float32l,
+        Float64l,
+        Int8sl,
+        Int8ul,
+        Int16sl,
+        Int16ul,
+        Int32sb,
+        Int32sl,
+        Int32ub,
+        Int32ul,
+        Int64sb,
+        Int64sl,
+        Int64ub,
+        Int64ul,
+        Struct,
+    )
     from elftools.elf.elffile import ELFFile
     from elftools.elf.sections import SymbolTableSection
-except ModuleNotFoundError:
-    print("Please execute the following command to install dependencies:")
-    print("pip install construct pyelftools cxxfilt")
-    exit(1)
+except ModuleNotFoundError as e:
+    print(f"Error:{e}.\nPlease execute the following command to install dependencies:")
+    print("pip install construct pyelftools cxxfilt lief ")
 
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
@@ -197,7 +217,7 @@ class ELFParser:
         )
 
         item_type = self.parse_die(type_die)
-        array = construct.Array(nums, item_type)
+        array = Array(nums, item_type)
         return array
 
     def parse_die(self, die):
@@ -220,9 +240,9 @@ class ELFParser:
             return self.typedef(die.attributes["DW_AT_name"].value.decode("utf-8"))
         elif die.tag == "DW_TAG_pointer_type":
             if self.info["bitwides"] == 32:
-                return construct.Int32ul
+                return Int32ul
             elif self.info["bitwides"] == 64:
-                return construct.Int64ul
+                return Int64ul
             else:
                 raise ValueError("Unsupported ELF class")
         elif die.tag == "DW_TAG_array_type":
@@ -262,28 +282,28 @@ class ELFParser:
             size = die.attributes["DW_AT_byte_size"].value
 
             unsigned_map = {
-                1: construct.Int8ul,
-                2: construct.Int16ul,
-                4: construct.Int32ul,
-                8: construct.Int64ul,
+                1: Int8ul,
+                2: Int16ul,
+                4: Int32ul,
+                8: Int64ul,
             }
             signed_map = {
-                1: construct.Int8sl,
-                2: construct.Int16sl,
-                4: construct.Int32sl,
-                8: construct.Int64sl,
+                1: Int8sl,
+                2: Int16sl,
+                4: Int32sl,
+                8: Int64sl,
             }
             double_map = {
-                2: construct.Float16l,
-                4: construct.Float32l,
-                8: construct.Float64l,
+                2: Float16l,
+                4: Float32l,
+                8: Float64l,
             }
             if "unsigned" in name:
                 return unsigned_map.get(size, None)
             elif "double" in name or "float" in name:
                 return double_map.get(size, None)
             elif "_Bool" in name:
-                return construct.Int8ul
+                return Int8ul
             elif "char" in name or "short" in name or "int" in name:
                 return signed_map.get(size, None)
 
@@ -325,7 +345,7 @@ class ELFParser:
                 member_type = self.parse_die(type_die)
                 members[member_name] = member_type
 
-            struct = construct.Struct(**members)
+            struct = Struct(**members)
             if not rets or all(ret.sizeof() != struct.sizeof() for ret in rets):
                 rets.add(struct)
 
@@ -386,3 +406,91 @@ class ELFParser:
                 rets.add(value)
 
         return enums.set_result(enum_name, rets)
+
+
+class LiefELF:
+    def __init__(self, filename):
+        self.elf = lief.parse(filename)
+        if not self.elf:
+            raise BaseException(f"Failed to parse ELF file: {filename}")
+
+        self.symbols = {sym.name.split(".")[0]: sym for sym in self.elf.symbols}
+        self.endian = (
+            "l"
+            if self.elf.abstract.header.endianness == lief.Header.ENDIANNESS.LITTLE
+            else "b"
+        )
+        self.architecture = self.elf.abstract.header.architecture
+        self.bits = 64 if self.elf.abstract.header.is_64 else 32
+
+    def get_symbol(self, symbol):
+        return self.symbols.get(symbol)
+
+    def read_symbol(
+        self, symbol, struct: Construct = None
+    ) -> Tuple[lief.Symbol, memoryview]:
+        sym = self.get_symbol(symbol)
+        if sym is None:
+            return None
+
+        data = self.read_from(sym.value, sym.size)
+        if struct:
+            data = struct.parse(data)
+        return sym, data
+
+    def read_from(self, addr, len=1) -> memoryview:
+        for section in self.elf.sections:
+            if section.type == lief.ELF.Section.TYPE.PROGBITS:
+                off = addr - section.virtual_address
+                if (
+                    section.virtual_address
+                    <= addr
+                    < section.virtual_address + section.size
+                    and section.size - off >= len
+                ):
+                    return section.content[off : off + len]
+
+        for segment in self.elf.segments:
+            if segment.type == lief.ELF.Segment.TYPE.LOAD:
+                off = addr - segment.virtual_address
+                if (
+                    segment.virtual_address
+                    <= addr
+                    < segment.virtual_address + segment.virtual_size
+                    and segment.virtual_size - off >= len
+                ):
+                    return segment.content[off : off + len]
+
+        return None
+
+    def read_string(self, addr) -> str:
+        """Read const string from ELF file"""
+        output = b""
+        while True:
+            c = self.read_from(addr, 1)
+            if c == b"\0":
+                break
+
+            output += c.tobytes()
+            addr += 1
+
+        return output.decode("utf-8")
+
+    def get_inttype(self) -> Construct:
+        return {
+            "32l": Int32sl,
+            "32b": Int32sb,
+            "64l": Int64sl,
+            "64b": Int64sb,
+        }.get(f"{self.bits}{self.endian}", Int32sl)
+
+    def get_pointer_type(self) -> Construct:
+        return {
+            "32l": Int32ul,
+            "32b": Int32ub,
+            "64l": Int64ul,
+            "64b": Int64ub,
+        }.get(f"{self.bits}{self.endian}", Int32ul)
+
+    def get_pointer_size(self):
+        return 8 if self.elf.abstract.header.is_64 else 4
--- tools/pynuttx/nxstub/utils.py
+++ tools/pynuttx/nxstub/utils.py
@@ -19,127 +19,13 @@
 ############################################################################
 
 try:
-    import lief
-    from construct import (
-        Array,
-        Construct,
-        Int8ul,
-        Int16ul,
-        Int32sb,
-        Int32sl,
-        Int32ub,
-        Int32ul,
-        Int64sb,
-        Int64sl,
-        Int64ub,
-        Int64ul,
-        Struct,
-    )
+    from construct import Array, Int8ul, Int16ul, Struct
 except ImportError:
-    print('Package missing, please do "pip install lief construct"')
+    print('Package missing, please do "pip install construct"')
 
-from typing import List, Tuple
+from typing import List
 
-
-class RegInfo:
-    def __init__(self, name, size, tcb_offset=0):
-        self.name = name
-        self.size = size
-        self.tcb_offset = tcb_offset
-
-    def __str__(self):
-        return f"{self.name}({self.size})"
-
-    def __repr__(self):
-        return f"REG({self.name}, {self.size}, {self.tcb_offset})"
-
-
-class LiefELF:
-    def __init__(self, filename):
-        self.elf = lief.parse(filename)
-        if not self.elf:
-            raise BaseException(f"Failed to parse ELF file: {filename}")
-
-        self.symbols = {sym.name.split(".")[0]: sym for sym in self.elf.symbols}
-        self.endian = (
-            "l"
-            if self.elf.abstract.header.endianness == lief.Header.ENDIANNESS.LITTLE
-            else "b"
-        )
-        self.architecture = self.elf.abstract.header.architecture
-        self.bits = 64 if self.elf.abstract.header.is_64 else 32
-
-    def get_symbol(self, symbol):
-        return self.symbols.get(symbol)
-
-    def read_symbol(
-        self, symbol, struct: Construct = None
-    ) -> Tuple[lief.Symbol, memoryview]:
-        sym = self.get_symbol(symbol)
-        if sym is None:
-            return None
-
-        data = self.read_from(sym.value, sym.size)
-        if struct:
-            data = struct.parse(data)
-        return sym, data
-
-    def read_from(self, addr, len=1) -> memoryview:
-        for section in self.elf.sections:
-            if section.type == lief.ELF.Section.TYPE.PROGBITS:
-                off = addr - section.virtual_address
-                if (
-                    section.virtual_address
-                    <= addr
-                    < section.virtual_address + section.size
-                    and section.size - off >= len
-                ):
-                    return section.content[off : off + len]
-
-        for segment in self.elf.segments:
-            if segment.type == lief.ELF.Segment.TYPE.LOAD:
-                off = addr - segment.virtual_address
-                if (
-                    segment.virtual_address
-                    <= addr
-                    < segment.virtual_address + segment.virtual_size
-                    and segment.virtual_size - off >= len
-                ):
-                    return segment.content[off : off + len]
-
-        return None
-
-    def read_string(self, addr) -> str:
-        """Read const string from ELF file"""
-        output = b""
-        while True:
-            c = self.read_from(addr, 1)
-            if c == b"\0":
-                break
-
-            output += c.tobytes()
-            addr += 1
-
-        return output.decode("utf-8")
-
-    def get_inttype(self) -> Construct:
-        return {
-            "32l": Int32sl,
-            "32b": Int32sb,
-            "64l": Int64sl,
-            "64b": Int64sb,
-        }.get(f"{self.bits}{self.endian}", Int32sl)
-
-    def get_pointer_type(self) -> Construct:
-        return {
-            "32l": Int32ul,
-            "32b": Int32ub,
-            "64l": Int64ul,
-            "64b": Int64ub,
-        }.get(f"{self.bits}{self.endian}", Int32ul)
-
-    def get_pointer_size(self):
-        return 8 if self.elf.abstract.header.is_64 else 4
+from nxelf.elf import LiefELF
 
 
 def get_ncpus(elf: LiefELF) -> int:
@@ -155,6 +41,19 @@ def get_regsize(elf: LiefELF) -> int:
     """Register size in context"""
     sym = elf.get_symbol("g_last_regs")
     return sym.size // get_ncpus(elf)
+
+
+class RegInfo:
+    def __init__(self, name, size, tcb_offset=0):
+        self.name = name
+        self.size = size
+        self.tcb_offset = tcb_offset
+
+    def __str__(self):
+        return f"{self.name}({self.size})"
+
+    def __repr__(self):
+        return f"REG({self.name}, {self.size}, {self.tcb_offset})"
 
 
 def get_tcbinfo(elf: LiefELF):
