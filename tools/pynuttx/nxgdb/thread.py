954bbd11750b3c13b30bee19cdb80056963c81b0
wangmingrong1 <wangmingrong1@xiaomi.com>
2025-04-17 15:23:56 +0800

    gdb setregs: If tcb is currently running, setregs is not performed.
    VELAPLATFO-59181
    
    Change-Id: I4b30da4c4da6bb1f4152eac1b34706f4f1cf12bd
    Signed-off-by: wangmingrong1 <wangmingrong1@xiaomi.com>
    (cherry picked from commit 84b629e8e45f6d6bef07c2e8899a14c9dc6bcbd1)

--- tools/pynuttx/nxgdb/thread.py
+++ tools/pynuttx/nxgdb/thread.py
@@ -311,7 +311,9 @@ class Nxthread(gdb.Command):
                     except gdb.error and UnicodeDecodeError:
                         gdb.write(f"Thread {i}\n")
 
-                    gdb.execute(f"setregs g_pidhash[{i}]->xcp.regs")
+                    if not utils.task_is_running(tcb):
+                        gdb.execute(f"setregs g_pidhash[{i}]->xcp.regs")
+
                     cmd_arg = ""
                     for cmd in arg[2:]:
                         cmd_arg += cmd + " "
@@ -343,7 +345,9 @@ class Nxthread(gdb.Command):
                         except gdb.error and UnicodeDecodeError:
                             gdb.write(f"Thread {i}\n")
 
-                        gdb.execute(f"setregs g_pidhash[{i}]->xcp.regs")
+                        if not utils.task_is_running(utils.get_tcb(i)):
+                            gdb.execute(f"setregs g_pidhash[{i}]->xcp.regs")
+
                         gdb.write(gdb.execute(f"{cmd}\n", to_string=True))
                         g_registers.restore()
 
