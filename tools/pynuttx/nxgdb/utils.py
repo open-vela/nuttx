d41f371d51cbe713f497246137518b484d30fbca
wangmingrong1 <wangmingrong1@xiaomi.com>
2025-04-24 17:48:23 +0800

    gdb plugin: Catch an unknown error and print the call stack
    VELAPLATFO-59270
    
    Change-Id: I3bd22c896ec7d18b440bcf20b25994342314a204
    Signed-off-by: wangmingrong1 <wangmingrong1@xiaomi.com>

--- tools/pynuttx/nxgdb/utils.py
+++ tools/pynuttx/nxgdb/utils.py
@@ -30,6 +30,7 @@ import math
 import re
 import shlex
 import sys
+import traceback
 from enum import Enum
 from pathlib import Path
 from typing import List, Optional, Union
@@ -352,7 +353,7 @@ def dont_repeat_decorator(func):
             self.dont_repeat()
             func(self, args, from_tty)
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"Error: {e}\n{traceback.format_exc()}")
 
     return wrapper
 
