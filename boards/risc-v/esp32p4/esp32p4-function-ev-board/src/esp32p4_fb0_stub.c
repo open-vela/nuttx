5494902e7fee75cf6654f83f1ca607b0c44c95bf
yaojingwei <yaojingwei@xiaomi.com>
2026-07-18 16:04:32 +0800

    feat(boards): add ESP32-P4 boards support
    Add board common code and esp32p4-function-ev-board (primary dev board).
    Also includes Waveshare PICO-WIFI and M5Stack Tab5 boards.
    
    Board sources: boot, bringup, GPIO, buttons, ethernet, reset,
    framebuffer stub, appinit.
    Configs: NSH, GPIO, I2C, SPI, Timer, Watchdog, PWM, ADC, Ethernet,
    USB Console, PSRAM, SPI Flash, TWAI, RMT, RTC, Crypto, PM.

---
+++ boards/risc-v/esp32p4/esp32p4-function-ev-board/src/esp32p4_fb0_stub.c
@@ -0,0 +1,48 @@
+/****************************************************************************
+ * esp32p4_fb0_stub.c - Minimal /dev/fb0 character device stub
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/fs/fs.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <syslog.h>
+
+static uint8_t g_fb0_dummy[64];
+
+static ssize_t fb0_read(FAR struct file *filep, FAR char *buffer,
+                        size_t buflen)
+{
+  size_t copylen = buflen < sizeof(g_fb0_dummy) ?
+                   buflen : sizeof(g_fb0_dummy);
+  memcpy(buffer, g_fb0_dummy, copylen);
+  return (ssize_t)copylen;
+}
+
+static ssize_t fb0_write(FAR struct file *filep,
+                         FAR const char *buffer, size_t buflen)
+{
+  size_t copylen = buflen < sizeof(g_fb0_dummy) ?
+                   buflen : sizeof(g_fb0_dummy);
+  memcpy(g_fb0_dummy, buffer, copylen);
+  return (ssize_t)copylen;
+}
+
+static const struct file_operations g_fb0_fops =
+{
+  .open    = NULL,
+  .close   = NULL,
+  .read    = fb0_read,
+  .write   = fb0_write,
+  .seek    = NULL,
+  .ioctl   = NULL,
+  .mmap    = NULL,
+  .truncate = NULL,
+  .poll    = NULL,
+};
+
+int esp_fb0_stub_register(void)
+{
+  return register_driver("/dev/fb0", &g_fb0_fops, 0666, NULL);
+}
