8dbe907a8461c3b6b5ceddf3c0fcf7a690df1ffd
Yan Zhang <yz471686525@gmail.com>
2026-08-01 17:21:07 +0800

    arch/arm/bk7258: add Beken BK7258 SoC support (NSH on CPU0/UART0)
    Add SoC support for the Beken BK7258, a dual-core Armv8-M STAR-MC1.
    
    NuttX runs on CPU0 with the console on UART0 and brings up NSH.
    
    Signed-off-by: Yan Zhang <yz471686525@gmail.com>

---
+++ arch/arm/src/bk7258/bk7258_timerisr.c
@@ -0,0 +1,59 @@
+/****************************************************************************
+ * arch/arm/src/bk7258/bk7258_timerisr.c
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.  The
+ * ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ ****************************************************************************/
+
+/* Porting reference: arch/arm/src/mps/mps_timer.c (armv8-m SysTick). */
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <arch/board/board.h>
+#include <nuttx/timers/arch_timer.h>
+
+#include "arm_internal.h"
+#include "systick.h"
+#include "nvic.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define SYSTICK_RELOAD ((BOARD_SYSTICK_CLOCK / CLK_TCK) - 1)
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: up_timer_initialize
+ *
+ * Description:
+ *   This function is called during start-up to initialize the timer
+ *   hardware (SysTick).
+ *
+ ****************************************************************************/
+
+void up_timer_initialize(void)
+{
+  putreg32(SYSTICK_RELOAD, NVIC_SYSTICK_RELOAD);
+  up_timer_set_lowerhalf(systick_initialize(true, BOARD_SYSTICK_CLOCK, -1));
+}
